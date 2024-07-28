47ac6bf6be3b23fa5cd0acaed73c627f207d7fe5
Mahmoud <mahmoudali77009@gmail.com>
2024-07-28 17:19:42 +0300

    finalUpdate

---
+++ app/src/main/java/com/example/ecomm/models/Product.java
@@ -0,0 +1,26 @@
+package com.example.ecomm.models;
+//represent the data model.
+public class Product {
+    private int id;
+    private String title;
+    private String price;
+    private String description;
+    private String image;
+
+    // Getters and Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getPrice() { return price; }
+    public void setPrice(String price) { this.price = price; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getImage() { return image; }
+    public void setImage(String image) { this.image = image; }
+}
+
