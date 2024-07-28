516e800e615bd47ce70a29c6b004e894a8a300fc
nada <dodzshaheen62@gmail.com>
2024-07-28 17:13:42 +0300

    description page

--- app/src/main/java/com/example/ecomm/MainActivity.java
+++ app/src/main/java/com/example/ecomm/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ecomm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+      Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+       // bt=findViewById(R.id.button);
+        //bt.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+            //public void onClick(View view) {
+              // Intent intent=new Intent(MainActivity.this,description.class);
+            //}
+        //});
+
+
+
+
+
 
     }
 }
